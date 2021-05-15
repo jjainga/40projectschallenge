69e10fb52ee4ab1359d85e30233a65d050fa7039
Joshua Jainga <jjainga@gmail.com>
2021-05-15 11:59:28 -0700

    added mission

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="jumbotron">
+          <h1 class="display-4">40 Project Challenge!</h1>
+          <p class="lead">To get myself back in the coding spirit I will be creating this app whcih will show off 40 different codings projects!</p>
+          <hr class="my-4"></hr>
+
+        </div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfnemlZq5EQZwF6_OfyMTIX3zM8vIfdOAUEw&usqp=CAU"  alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://images.app.goo.gl/wn1gSJmdjkZ2friJA"
           target="_blank"
           rel="noopener noreferrer"
         >
