5f2dc853529d8470f94e1c2c004e052e36f42034
Joshua Jainga <jjainga@gmail.com>
2021-05-19 08:50:18 -0700

    filtered zero

---
+++ src/components/button/numberButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const NumberButton = (props) => {
+
+ 
+    console.log(props.props)
+    return (
+        <button type="button" className="btn btn-primary btn-lg m-3" id={props.props.int} name={props.props.number} >{props.props.int}</button>
+          )
+}
+export default NumberButton;
